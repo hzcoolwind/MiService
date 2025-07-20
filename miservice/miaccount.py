cc1d8a12bfaff41630077ddc9b8105c983a8e3ad
TangJiahui <tangjiahui0610@gmail.com>
2025-07-20 11:58:15 +0800

    add verify

--- miservice/miaccount.py
+++ miservice/miaccount.py
@@ -4,6 +4,7 @@ from json import dumps, loads
 from os import remove, path
 from random import sample
 from string import ascii_letters, digits
+import time
 from urllib import parse
 from aiofiles import open as aio_open
 
@@ -19,6 +20,7 @@ class MiTokenStore:
 
     def __init__(self, token_path):
         self.token_path = token_path
+        print("token_path: ", self.token_path)
 
     async def load_token(self):
         if path.isfile(self.token_path):
@@ -48,6 +50,9 @@ class MiAccount:
         self.password = password
         self.token_store = MiTokenStore(token_store) if isinstance(token_store, str) else token_store
         self.token = None
+        # 新增tgm
+        self.verify = None
+        self.identity_session = None
 
     def request(self, url, method='GET', **kwargs):
         if self._session:
@@ -81,6 +86,15 @@ class MiAccount:
                     'hash': md5(self.password.encode()).hexdigest().upper()
                 }
                 resp = await self._serviceLogin('serviceLoginAuth2', data)
+                
+                # 新增tgm
+                self.verify = resp.get("notificationUrl")
+                if self.verify[:4] != 'http':
+                    self.verify = f'https://account.xiaomi.com{self.verify}'
+
+                if not resp.get("location", ''):
+                    return False
+                
                 if resp['code'] != 0:
                     raise Exception(resp)
 
@@ -125,7 +139,7 @@ class MiAccount:
     async def mi_request(self, sid, url, data, headers, relogin=True):
         if self.token is None and self.token_store is not None:
             self.token = await self.token_store.load_token()
-        if (self.token and sid in self.token) or await self.login(sid):  # Ensure login
+        if (self.token and sid in self.token) or await self.do_login(sid):  # Ensure login
             cookies = {'userId': self.token['userId'], 'serviceToken': self.token[sid][1]}
             content = data(self.token, cookies) if callable(data) else data
             method = 'GET' if data is None else 'POST'
@@ -150,3 +164,88 @@ class MiAccount:
         else:
             resp = "Login failed"
         raise Exception(f"Error {url}: {resp}")
+    
+
+    # 以下新增
+    async def do_login(self, sid) -> bool:
+        try:
+            result = await self.login(sid)
+            if not result:       
+                if self.verify:
+                    print("need verify: ", self.verify)
+                    ticket = input("请输入验证码：")
+                    resp = await self.verify_ticket(self.verify, ticket)
+                    location = resp.get('location', '')
+                    if location:
+                        resp = await self.session.get(location, allow_redirects=True)
+                        resp = await self._serviceLogin(f'serviceLogin?sid={sid}&_json=true')                       
+                        location = data.get('location', '')
+
+                        if not location:
+                            if resp['code'] != 0:
+                                data = {
+                                    '_json': 'true',
+                                    'qs': resp['qs'],
+                                    'sid': resp['sid'],
+                                    '_sign': resp['_sign'],
+                                    'callback': resp['callback'],
+                                    'user': self.username,
+                                    'hash': md5(self.password.encode()).hexdigest().upper()
+                                }
+                                resp = await self._serviceLogin('serviceLoginAuth2', data)
+                            if not resp.get('location', ''):
+                                return False
+                            location = resp["location"]
+                                
+                        self.token['userId'] = resp['userId']
+                        self.token['passToken'] = resp['passToken']
+
+                        serviceToken = await self._securityTokenService(resp['location'], resp['nonce'], resp['ssecurity'])
+                        self.token[sid] = (resp['ssecurity'], serviceToken)
+                        if self.token_store:
+                            await self.token_store.save_token(self.token)
+                        return True
+                        
+                return False
+        
+            return result
+        except Exception as e:
+            _LOGGER.exception(f"Can't login to Mi Cloud: {e}")
+            return False
+        
+    async def check_identity_list(self, url, path='identity/authStart'):
+        if path not in url:
+            return None
+        async with self.request(url.replace(path, 'identity/list'), "GET") as r:
+            self.identity_session = r.cookies.get('identity_session')
+            if not self.identity_session:
+                return False
+            raw = await r.read()
+            resp = loads(raw[11:])            
+            flag= resp.get('flag', 4)
+            options = resp.get('options', [flag])
+            return options
+
+    async def verify_ticket(self, url, ticket):
+        url = self.verify
+        if not url:
+            return False
+        options = await self.check_identity_list(url) or []
+        for flag in options:
+            api = {
+                4: 'https://account.xiaomi.com/identity/auth/verifyPhone',
+                8: 'https://account.xiaomi.com/identity/auth/verifyEmail',
+            }.get(flag)
+            if not api:
+                continue
+
+            data={'_flag': flag, 'ticket': ticket, 'trust': 'true', '_json': 'true'}
+            cookies={'identity_session': self.identity_session}
+            headers={"User-Agent":'APP/com.xiaomi.mihome APPV/6.0.103 iosPassportSDK/3.9.0 iOS/14.4 miHSTS'}
+            async with self.request(api + f"?_dc={int(time.time() * 1000)}", 'POST', data=data, cookies=cookies, headers=headers) as r:
+                raw = await r.read()
+                resp = loads(raw[11:])
+                if resp.get('code') == 0:
+                    self.identity_session = None
+                    return resp
+        return {}
